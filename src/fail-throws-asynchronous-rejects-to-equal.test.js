ccae909865bdcae3f6d58b0548358d998eb019da
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-10-19 18:38:00 +0100

    show that await expect() works like return expect()

--- src/fail-throws-asynchronous-rejects-to-equal.test.js
+++ src/fail-throws-asynchronous-rejects-to-equal.test.js
@@ -2,8 +2,14 @@ async function asyncThrowOrNot() {
   return 'success';
 }
 
-it('should throw if passed true', async () => {
+it('should throw if passed true return expect()', async () => {
   return expect(asyncThrowOrNot(true)).rejects.toEqual(
+    new Error('shouldThrow was true')
+  );
+});
+
+it('should throw if passed true await expect()', async () => {
+  await expect(asyncThrowOrNot(true)).rejects.toEqual(
     new Error('shouldThrow was true')
   );
 });
